e78a743be61058d9e50a866e14c43d71bd072a28
Deepak Neralla <deepak.neralla@gmail.com>
2013-03-31 18:09:32 -0500

    Initial push

--- MP4Hbase/src/HbaseProblem.java
+++ MP4Hbase/src/HbaseProblem.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
@@ -7,6 +6,11 @@ import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HbaseProblem {
 
@@ -31,7 +35,6 @@ public class HbaseProblem {
 		}
 	}
 
-	
 	public static void addRecord(String tableName, String rowKey,
 			String family, String qualifier, String value) throws Exception {
 		try {
@@ -48,10 +51,50 @@ public class HbaseProblem {
 		}
 	}
 
-	public void addDataFromFile(String dataFileName, String tableName)
-			throws Exception {
-		// ToDo read file name and add data liek below
-		// HbaseProblem.addRecord(tableName, "zkb", "background", "id", "5");
+	public void addDataFromFile(String dataFileName, String tableName,
+			String delimiter, String[] colFamilies) throws Exception {
+
+		BufferedReader br = new BufferedReader(new FileReader(dataFileName));
+		String data = null;
+		String columns = br.readLine();
+		String column[] = null;
+
+		if (columns != null)
+			column = columns.split(delimiter);
+
+		int n = column == null ? 0 : column.length;
+		Map<String, String> colColFamilyMapping = new HashMap<String, String>();
+		for (int i = 0; i < n; i++)
+			if (i <= 3)
+				colColFamilyMapping.put(column[i], colFamilies[0]);
+			else
+				colColFamilyMapping.put(column[i], colFamilies[1]);
+
+		int dataRowNumber = 1;
+
+		while ((data = br.readLine()) != null) {
+			{
+
+				String rowKey = hash(dataRowNumber);
+				String splitData[] = data.split(delimiter);
+
+				for (int i = 0; i < n; i++)
+					addRecord(tableName, rowKey,
+							colColFamilyMapping.get(column[i]), column[i],
+							splitData[i]);
+
+				dataRowNumber++;
+
+			}
+
+		}
+
+		br.close();
+
+	}
+
+	String hash(int dataRowNumber) {
+		return "-" + dataRowNumber + "-";
 	}
 
 	public static void main(String[] args) {
@@ -61,7 +104,7 @@ public class HbaseProblem {
 				String[] columnFamilys = { "background", "powers" };
 				HbaseProblem hProblem = new HbaseProblem();
 				hProblem.createTable(tableName, columnFamilys);
-				hProblem.addDataFromFile(args[0], tableName);
+				hProblem.addDataFromFile(args[0], tableName, " ", columnFamilys);
 			} else
 				System.out.println("Pass data file argument atleast");
 
